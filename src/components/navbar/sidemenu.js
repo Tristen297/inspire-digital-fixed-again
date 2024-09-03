188b53d4df148453826cb6f392bf35fd8dc268ac
Tristen Dean <103838311+Tristen297@users.noreply.github.com>
2024-09-03 00:35:37 -0400

    additional commit

--- src/components/navbar/sidemenu.js
+++ src/components/navbar/sidemenu.js
@@ -2,7 +2,9 @@ import { useState } from "react";
 import "../../styles/navbar/navbar.css";
 import menuIcon from "../../images-and-icons/icon.svg";
 import locationIcon from "../../images-and-icons/location-icon.svg";
-import logoIcon from "../../images-and-icons/inspire-icon.png"
+import phoneIcon from "../../images-and-icons/phone-icon.svg";
+import mailIcon from "../../images-and-icons/mail-icon.svg";
+import logoIcon from "../../images-and-icons/inspire-icon.png";
 
 function Sidemenu() {
     const [isOpen, setIsOpen] = useState(false);
@@ -26,15 +28,32 @@ function Sidemenu() {
                 
                 <div className="menu-section">
                     <h2>About Us</h2>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+                    <p>
+                        At Inspire Digital, we craft visually stunning and highly functional websites tailored to your brand. Our team is dedicated to helping your business thrive online with custom design and development solutions.
+                    </p>
                     <button className="menu-button">Contact Us</button>
                 </div>
 
                 <div className="menu-section">
                     <h2>Contact Us</h2>
-                    <p><i className="fa fa-map-marker"></i> Valentin, Street Road 24, New York</p>
-                    <p><i className="fa fa-phone"></i> +000 123 (456) 789</p>
-                    <p><i className="fa fa-envelope"></i> runokcontact@gmail.com</p>
+                    <p>
+                        <span className="icon-container">
+                            <img src={locationIcon} alt="Location Icon" className="icon"/>
+                        </span> 
+                        2272 Erie Ave Ste 219, Cincinnati
+                    </p>
+                    <p>
+                        <span className="icon-container">
+                            <img src={phoneIcon} alt="Phone Icon" className="icon"/>
+                        </span> 
+                        +1 (812) 260-1399
+                    </p>
+                    <p>
+                        <span className="icon-container">
+                            <img src={mailIcon} alt="Mail Icon" className="icon"/>
+                        </span> 
+                        contact@myinspiredigital.com
+                    </p>
                 </div>
 
                 <div className="social-icons">
