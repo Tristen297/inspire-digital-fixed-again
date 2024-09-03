2df6656a86e414e099bdaeb83fc029ba54e7ff32
Tristen Dean <103838311+Tristen297@users.noreply.github.com>
2024-09-03 01:43:33 -0400

    finished navbar & associated sidemenu

--- src/components/navbar/navbar.js
+++ src/components/navbar/navbar.js
@@ -1,10 +1,25 @@
 import "../../styles/navbar/navbar.css";
 import Sidemenu from "./sidemenu";
+import logoIcon from "../../images-and-icons/inspire-icon.png";
 
 function Navbar() {
     return(
         <div className="navbar-content">
-            <Sidemenu />
+            <div className="sidemenu-container">
+                <Sidemenu />
+            </div>
+            <div className="main-nav-content">
+                <div className="nav-logo-full">
+                    <img src={logoIcon} alt="Logo" className="menu-logo" />
+                    <h1>Inspire Digital</h1>
+                </div>
+                <nav className="nav-links">
+                    <a href="#home">Home</a>
+                    <a href="#services">Services</a>
+                    <a href="#about">About Us</a>
+                    <a href="#contact">Contact</a>
+                </nav>
+            </div>
         </div>
     )
 }
