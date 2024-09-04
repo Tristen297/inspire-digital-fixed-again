5243cd39e9fa5abeb5c66d3ca1533ebfca7fa03a
Tristen Dean <103838311+Tristen297@users.noreply.github.com>
2024-09-04 01:43:14 -0400

    added outline around c in the word creative

--- src/components/home/hero.js
+++ src/components/home/hero.js
@@ -10,12 +10,14 @@ function Hero() {
                 <a href="https://instagram.com" target="_blank" rel="noopener noreferrer">Instagram</a>
             </div>
             <div className="hero-picture">
-                <h1 className="hero-text">CREATIVE</h1>
-                <h2 className="agency-text">STUDIO</h2> 
+                <h1 className="hero-text">
+                    <span className="outline-letter">C</span>REATIVE
+                </h1>
+                <h2 className="agency-text">AGENCY</h2>
                 <img src={heroPic} alt="man working on computer" />
             </div>
             <div className="hero-content-text">
-                <p className="welcome-text">WELCOME TO OUR STUDIO</p>
+                <p className="welcome-text">WELCOME TO OUR AGENCY</p>
                 <h1 className="main-heading">Digital Creative <br /> Studio</h1>
                 <p className="description">Over 15 years in the game & build successful, profitable products.</p>
                 <button className="cta-button">Get Started Now</button>
