6939d79d5b2720c82b562e0d5c171e5bcb63af8b
Tristen Dean <103838311+Tristen297@users.noreply.github.com>
2024-09-07 16:11:44 -0400

    first round of resizing on navbar

--- src/components/navbar/navbar.js
+++ src/components/navbar/navbar.js
@@ -1,9 +1,16 @@
+import React, { useState } from 'react';
 import "../../styles/navbar/navbar.css";
 import Sidemenu from "./sidemenu";
 import logoIcon from "../../images-and-icons/inspire-icon.png";
 
 function Navbar() {
-    return(
+    const [isOpen, setIsOpen] = useState(false);
+
+    const toggleMenu = () => {
+        setIsOpen(!isOpen);
+    };
+
+    return (
         <div className="navbar-content">
             <div className="sidemenu-container">
                 <Sidemenu />
@@ -13,15 +20,23 @@ function Navbar() {
                     <img src={logoIcon} alt="Logo" className="menu-logo" />
                     <h1>Inspire Digital</h1>
                 </div>
-                <nav className="nav-links">
+
+                <div className={`nav-links ${isOpen ? 'open' : ''}`}>
                     <a href="./">Home</a>
                     <a href="./">Services</a>
                     <a href="./">About Us</a>
                     <a href="./">Contact</a>
-                </nav>
+                </div>
+
+                {/* Hamburger Icon */}
+                <div className="hamburger" onClick={toggleMenu}>
+                    <span className="bar"></span>
+                    <span className="bar"></span>
+                    <span className="bar"></span>
+                </div>
             </div>
         </div>
-    )
+    );
 }
 
 export default Navbar;
